7be0366cd1b48d3e8bfc3dd7e1dcfdb71c7fab64
EhoodGoren <ehoodgoren@gmail.com>
2021-08-31 00:45:29 +0300

    Displays all three previous functions' values together

--- grades.js
+++ grades.js
@@ -4,7 +4,7 @@ const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
 //from here on are some base functions which are used in more than one fucntion
 function lowestGrade(grades){
     let lowestGrade=100;
-    for(let i=0; i<grades.length; i++){
+    for(let i=0; i < grades.length; i++){
         if(grades[i]<lowestGrade){
             lowestGrade=grades[i];
         }
@@ -14,7 +14,7 @@ function lowestGrade(grades){
 
 function highestGrade(grades){
     let highestGrade=0;
-    for(let i=0; i<grades.length; i++){
+    for(let i=0; i < grades.length; i++){
         if(grades[i]>highestGrade){
             highestGrade=grades[i];
         }
@@ -36,7 +36,6 @@ function arrayAscending(grades){
 function gradeRange(grades){
     return highestGrade(grades)-lowestGrade(grades);
 }
-console.log(gradeRange(grades));
 
 
 function medianGrade(grades){
@@ -48,11 +47,9 @@ function medianGrade(grades){
         return grades[(grades.length-1)/2];
     }
 }
-//console.log(medianGrade(grades));
-console.log(grades);
+
 
 function secondHalfGrades(grades){
-    //console.log(grades)
     let secondHalfArr;
     if(grades.length%2 === 0){
         grades.splice(0,grades.length/2)
@@ -64,4 +61,8 @@ function secondHalfGrades(grades){
     }
     return gradeRange(secondHalfArr);
 }
+
+console.log(gradeRange(grades));
+//console.log(medianGrade(grades));
 console.log(secondHalfGrades(grades));
+//console.log("Stats: \n Range: " + gradeRange(grades) + "\n Median: " + medianGrade(grades) + "\n Half Range: " + secondHalfGrades(grades));
