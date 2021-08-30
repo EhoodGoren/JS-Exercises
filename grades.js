9c2864ae2b34113a54958b5ca61f897c184ef9cc
EhoodGoren <ehoodgoren@gmail.com>
2021-08-30 23:25:58 +0300

    split functions lowest and highest grade
    gradeRange now only returns the difference between highest and lowest. separated for future functions use.

--- grades.js
+++ grades.js
@@ -1,17 +1,34 @@
 "use strict";
-//const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
-const grades = [1,100,45];
-function gradeRange(grades){
+const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
+
+function lowestGrade(grades){
     let lowestGrade=100;
-    let highestGrade=0;
     for(let i=0; i<grades.length; i++){
         if(grades[i]<lowestGrade){
             lowestGrade=grades[i];
         }
+    }
+    return lowestGrade;
+}
+
+function highestGrade(grades){
+    let highestGrade=0;
+    for(let i=0; i<grades.length; i++){
         if(grades[i]>highestGrade){
             highestGrade=grades[i];
         }
     }
-    return (lowestGrade + " - " + highestGrade);
+    return highestGrade;
+}
+
+function gradeRange(grades){
+    return highestGrade(grades)-lowestGrade(grades);
 }
 console.log(gradeRange(grades));
+
+
+function medianGrade(grades){
+    let ascArr=[];
+    for(i=0; i<grades.length; i++){
+    }
+}
