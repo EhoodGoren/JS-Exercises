4da53b9334b755d876a66a6304ec99fc284c1be9
EhoodGoren <ehoodgoren@gmail.com>
2021-08-30 23:42:31 +0300

    Added a new function, arrayAscending
    arrayAscending can be used for more than one instance, so it was also move to the "base functions" part of the code.

--- grades.js
+++ grades.js
@@ -1,6 +1,7 @@
 "use strict";
 const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
 
+//from here on are some base functions which are used in more than one fucntion
 function lowestGrade(grades){
     let lowestGrade=100;
     for(let i=0; i<grades.length; i++){
@@ -20,6 +21,17 @@ function highestGrade(grades){
     }
     return highestGrade;
 }
+function arrayAscending(grades){
+    let gradesLength=grades.length;
+    let ascArr=[];
+    for(let i=0; i<gradesLength; i++){
+        ascArr[i]=lowestGrade(grades);
+        grades.splice(grades.indexOf(lowestGrade(grades)),1);
+    }
+    return ascArr;
+}
+//end of said base functions
+
 
 function gradeRange(grades){
     return highestGrade(grades)-lowestGrade(grades);
@@ -28,7 +40,6 @@ console.log(gradeRange(grades));
 
 
 function medianGrade(grades){
-    let ascArr=[];
-    for(i=0; i<grades.length; i++){
-    }
 }
+
+console.log(medianGrade(grades));
