71a35e62ba354bae26237268ad00f7fe2ced0640
EhoodGoren <ehoodgoren@gmail.com>
2021-08-31 22:22:41 +0300

    Added checking for a bunch of cities.
    Returns some city names, and no city name if doesn't match one of the predefined ones

---
+++ cityTrivia.js
@@ -0,0 +1,25 @@
+"use strict"
+
+function guessCity(capital, coastal, famous, ancient){
+    if(capital && !coastal && famous && ancient){
+        return "Jerusalem";
+    }
+    if(!capital && coastal && famous && !ancient){
+        return "Tel Aviv";
+    }
+    if(!capital && coastal && !famous && ancient){
+        return "Acre";
+    }
+    if(!capital && !coastal && !famous && ancient){
+        return "Katzrin";
+    }
+    if(!capital && coastal && !famous && !ancient){
+        return "Zikim";
+    }
+    if(!capital, !coastal, !famous, !ancient){
+        return "Musmus";
+    }
+    else{
+        return "No match for a city";
+    }
+}
