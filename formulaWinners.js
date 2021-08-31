726043a16ef8adb508c1f95b1a04f9d919c030f5
EhoodGoren <ehoodgoren@gmail.com>
2021-08-31 23:58:20 +0300

    Used another algorithm, much shorter

--- formulaWinners.js
+++ formulaWinners.js
@@ -25,47 +25,13 @@ const formula1Champions = [
     "Hamilton"
 ]
 
-let winners = {
-    "Schumacher":0,
-    "Alonso":0,
-    "Räikkönen":0,
-    "Hamilton":0,
-    "Button":0,
-    "Vettel":0,
-    "Rosberg":0,
-}
+let winners = {}
 
 function countWin(driver) {
-    switch(driver){
-        case "Schumacher":
-            winners.Schumacher++
-            break;
-
-        case "Alonso":
-            winners.Alonso++
-            break;
-
-        case "Räikkönen":
-            winners.Räikkönen++
-            break;
-
-        case "Hamilton":
-            winners.Hamilton++
-            break;
-
-        case "Button":
-            winners.Button++
-            break;
-                
-        case "Vettel":
-            winners.Vettel++
-            break;
-
-        case "Rosberg":
-            winners.Rosberg++
-            break;            
+    if(!winners.hasOwnProperty(driver)){
+        winners[driver]=0;
     }
-    return winners[driver];
+    winners[driver]++;
 }
 
 formula1Champions.forEach(countWin); //forEach runs the countWin function on each of it's elements (the function input is the element).
